01df9339ecea5b5097db382650e48956010224b5
Nicolas Kolumbic <nicolaskolumbic@hotmail.com>
2023-09-18 12:26:09 -0300

    base components

---
+++ repo/src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
